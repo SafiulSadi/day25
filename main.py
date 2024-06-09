b7287aba969f0696a57daf758ff9df7ae92cd189
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2024-06-10 05:21:49 +0600

    day 25 add fahrenheit temp commit

--- main.py
+++ main.py
@@ -14,13 +14,28 @@
 #     print(temperatures)
 import pandas
 data = pandas.read_csv('weather_data.csv')
-print(data["temp"])
+# print(data["temp"])
 
+# print((data.temp.to_list()))
 
+data_dict = data.to_dict()
+# print(data_dict)
 
-
-
-
+temp_list = data["temp"].to_list()
+sum = 0
+# print(len(temp_list))
+for i in temp_list:
+    sum += i
+avg_temp = sum/len(temp_list)
+# print(round(avg_temp,2))
+# print(data['temp'].mean())
+# print(data.temp.max())
+# print(data.condition)
+# Get data in Row
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
+monday = data[data.day == "Monday"]
+print(monday.condition)
 
 
 
